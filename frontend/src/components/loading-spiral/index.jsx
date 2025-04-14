e0c9e3c8373e6559b54efecec942741af81bef8b
Leonardo Neto <leonardoneto0710@gmail.com>
2025-04-14 20:35:38 -0300

    feat: added LoadingSpiral component

---
+++ frontend/src/components/loading-spiral/index.jsx
@@ -0,0 +1,5 @@
+export function LoadingSpiral() {
+  return (
+    <div className="inline-block animate-spin rounded-full h-32 w-32 border-5 border-white border-t-transparent p-3 m-2"></div>
+  )
+}
